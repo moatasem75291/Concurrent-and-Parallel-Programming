1ec3bf46535d7351e191af1287361108958b00de
Mo3tsm <mo5les75291@gmail.com>
2024-05-12 01:20:28 +0300

    Refactor YahooFinanceWorker to use Queue in multiprocessing

--- chapter_1/Wikipedia Reader/YahooFinanceWorker.py
+++ chapter_1/Wikipedia Reader/YahooFinanceWorker.py
@@ -2,29 +2,49 @@ import threading
 import time, random
 import requests
 from lxml import etree
+from multiprocessing import Queue
+from typing import Optional
 
 
-class YahooFinanceWorker(threading.Thread):
-    def __init__(self, symbol, **kwargs):
-        super(YahooFinanceWorker, self).__init__(**kwargs)
-        self._symbol = symbol
-        base_url = "https://finance.yahoo.com/quote/"
-        self._url = f"{base_url}{self._symbol}"
+class YahooFinanceScheduler(threading.Thread):
+    def __init__(self, input_queue: Queue, **kwargs) -> None:
+        super(YahooFinanceScheduler, self).__init__(**kwargs)
+        self._input_queue: Queue = input_queue
         self.start()
 
-    def _extract_price(self):
-        time.sleep(random.randint(30, 60))
+    def run(self) -> None:
+        while True:
+            val: str = self._input_queue.get()
+            if val == "DONE":
+                break
+
+            yahooFinanceWorker: YahooFinanceWorker = YahooFinanceWorker(symbol=val)
+            price: Optional[str] = yahooFinanceWorker.extract_price()
+            print("{0}: {1}".format(val, price))
+
+            time.sleep(random.randint(1, 3))
+
+
+class YahooFinanceWorker:
+    def __init__(self, symbol: str) -> None:
+        self._symbol: str = symbol
+        base_url: str = "https://finance.yahoo.com/quote/"
+        self._url: str = f"{base_url}{self._symbol}"
+
+    def extract_price(self) -> Optional[str]:
+
         response = requests.get(self._url)
         if response.status_code != 200:
             print(f"Couldn't get data for {self._symbol}")
         else:
-            parser = etree.HTMLParser()
-            tree = etree.fromstring(response.text, parser)
-            xpath_expr = "//*[@id='nimbus-app']/section/section/section/article/section[1]/div[2]/div[1]/section/div/section[1]/div[1]/fin-streamer[1]"
-            element = tree.xpath(xpath_expr)[0]
-            price = element.find(".//span").text
+            try:
+                parser = etree.HTMLParser()
+                tree = etree.fromstring(response.text, parser)
+                xpath_expr = "//*[@id='nimbus-app']/section/section/section/article/section[1]/div[2]/div[1]/section/div/section[1]/div[1]/fin-streamer[1]"
+                element = tree.xpath(xpath_expr)[0]
+                price = element.find(".//span").text
 
-            print(f"{self._symbol}: {price}")
-
-    def run(self):
-        self._extract_price()
+                float(price.replace(",", ""))
+            except:
+                return None
+            return price
--- chapter_1/Wikipedia Reader/main.py
+++ chapter_1/Wikipedia Reader/main.py
@@ -1,21 +1,31 @@
 import time
 from WikipediaReader import WikipediaReader
-from YahooFinanceWorker import YahooFinanceWorker
+from YahooFinanceWorker import YahooFinanceScheduler
+from multiprocessing import Queue
 import itertools
+
+num_yahoo_finance_worker = 17
 
 
 def main():
     current_time = time.time()
+    symbol_queue = Queue()
 
     wiki_reader = WikipediaReader()
-    wiki_workers = []
-    symbols = wiki_reader.get_sp_500_companies()
-    for symbol in itertools.islice(symbols, 100):
-        worker = YahooFinanceWorker(symbol)
-        wiki_workers.append(worker)
 
-    for worker in wiki_workers:
-        worker.join()
+    yahoo_scheduler_threads = []
+    for _ in range(num_yahoo_finance_worker):
+        yahooFinanceScheduler = YahooFinanceScheduler(input_queue=symbol_queue)
+        yahoo_scheduler_threads.append(yahooFinanceScheduler)
+
+    for symbol in wiki_reader.get_sp_500_companies():
+        symbol_queue.put(symbol)
+
+    for _ in range(len(yahoo_scheduler_threads)):
+        symbol_queue.put("DONE")
+
+    for i in range(len(yahoo_scheduler_threads)):
+        yahoo_scheduler_threads[i].join()
 
     print(f"Execution time: {time.time() - current_time}")
 
